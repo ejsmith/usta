cd02dc2a2ddbce0eeb278fa9469c4735e88dd711
Eric J. Smith <eric@codesmithtools.com>
2015-05-17 17:31:37 -0500

    Some progress on scraping.

--- scrape.js
+++ scrape.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var geocoder = require('geocoder');
+var async = require('async');
 
 var baseUrl = 'http://tennislink.usta.com';
 var url = baseUrl + '/Tournaments/Schedule/SearchResults.aspx?Action=2&SectionDistrict=8096&Zip=75044&Division=D5004&QuickSearch=3&Sanctioned=0&State=TX';
@@ -13,7 +15,7 @@ request(url, function(error, response, html) {
         var rows = $('div.CommonTable > table > tr:nth-child(1n + 2)');
         rows.each(function(i, elem) {
             var tournament = {};
-            
+
             var nameValue = $('td > a:nth-child(1)', this).text().trim();
             var nameParts = $('td > a:nth-child(1)', this).text().split('(');
             var m;
@@ -25,11 +27,32 @@ request(url, function(error, response, html) {
             tournament.link = baseUrl + $('td:nth-child(5) > a', this).attr('href').trim();
             tournament.location = $('td:nth-child(3) > a:nth-child(4)', this).attr('href').trim().split('=')[1];;
             tournament.date = $('td:nth-child(1)', this).text().trim();
-            
+
             tournaments[tournament.id] = tournament;
         });
-        
-        console.log(tournaments);
-        console.log('count: ' + Object.keys(tournaments).length);
+
+        console.log('before');
+        async.each(tournaments, function(tournament, callback) {
+          console.log('hi');
+          console.log('geo id=' + tournament.id);
+          geocoder.geocode(tournament.location, function(err, data) {
+            console.log('got result');
+            if (err)
+              console.log(err);
+
+            if (data && data.results && data.results.length > 0)
+              tournament.geo = data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng;
+
+            callback();
+          });
+        }, function(err) {
+          console.log('done');
+            if (err) {
+              console.log('A file failed to process');
+            } else {
+              console.log(tournaments);
+              console.log('count: ' + Object.keys(tournaments).length);
+            }
+        });
     }
 });
